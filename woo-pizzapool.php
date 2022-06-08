464b79a9699fcb1904eec334e05698fba32e4665
khalidwebmail <csewebmail@gmail.com>
2022-06-08 23:13:47 +0600

    initail commit

---
+++ woo-pizzapool.php
@@ -0,0 +1,25 @@
+<?php
+
+/**
+ * Woo Pizzapool
+ *
+ * @package           Woo Pizzapool
+ * @author            Khalid Ahmed
+ * @copyright         2021 Khalid Ahmed
+ * @license           GPL-2.0-or-later
+ *
+ * @wordpress-plugin
+ * Plugin Name:       Woo Pizzapool
+ * Plugin URI:        https://example.com/plugin-name
+ * Description:       Manage diffrent types customer who came shopping in our store
+ * Version:           1.0.0
+ * Author:            Khalid Ahmed
+ * Author URI:        https://example.com
+ * Text Domain:       woo-pizzapool
+ * License:           GPL v2 or later
+ * License URI:       http://www.gnu.org/licenses/gpl-2.0.txt
+ */
+
+if( ! defined( 'ABSPATH' ) ) {
+	exit;
+}
